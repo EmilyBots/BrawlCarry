35c34bf20553f5f087ca2eda962211cc26cc8b46
rayan007-beep <rayanfautario85@gmail.com>
2026-05-18 21:06:11 +0200

    Update emoji IDs for order buttons

--- src/commands/admin/order_panel.js
+++ src/commands/admin/order_panel.js
@@ -27,7 +27,7 @@ const rankedPanelCmd = {
         .setCustomId('ranked_order_btn')
         .setLabel('Ranked Order')
         .setStyle(ButtonStyle.Primary)
-        .setEmoji({ name: 'masters', id: '1506001723657945262' })
+        .setEmoji({ name: 'master', id: '1491521740860428459' })
     );
 
     await interaction.channel.send({ embeds: [e, ...extraEmbeds], components: [view] });
@@ -60,7 +60,7 @@ const prestigePanelCmd = {
         .setCustomId('prestige_order_btn')
         .setLabel('Prestige Order')
         .setStyle(ButtonStyle.Secondary)
-        .setEmoji({ name: 'copyright', id: '1505997750620131408' })
+        .setEmoji({ name: 'copyright', id: '1485657838897467534' })
     );
 
     await interaction.channel.send({ embeds: [e, ...extraEmbeds], components: [view] });

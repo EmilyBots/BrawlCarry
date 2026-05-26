7df400de9bcbb9322f259bb44c92f144c8b394c3
rayan007-beep <rayanfautario85@gmail.com>
2026-05-26 11:51:07 +0200

    Update order_panel.js

--- src/commands/admin/order_panel.js
+++ src/commands/admin/order_panel.js
@@ -146,7 +146,7 @@ const supportThreadChannelCmd = {
       .setColor(0x5865F2)
       .setDescription(
         '# <:reply:1507680110843658260> Support Thread Channel\n' +
-        '### <:Boost:1508378809676861573> All private support tickets created by members will appear under this channel. <:info:1508767700329959545>'
+        '### <:Boost:1508378809676861573> All private support tickets created by members will appear under this channel <:info:1508767700329959545>'
       )
       .setThumbnail(BOT_LOGO);
 
@@ -161,4 +161,35 @@ const supportThreadChannelCmd = {
   },
 };
 
-module.exports = [rankedPanelCmd, prestigePanelCmd, rankedThreadChannelCmd, prestigeThreadChannelCmd, supportThreadChannelCmd];
+// ── 4ccount Thread Channel info panel ────────────────────────────────────────
+const accountThreadChannelCmd = {
+  data: new SlashCommandBuilder()
+    .setName('account_thread_panel')
+    .setDescription('Post the 4ccount Thread Channel info panel in this channel')
+    .setDefaultMemberPermissions(0x10),
+
+  async execute(interaction) {
+    const BOT_LOGO = 'https://cdn.discordapp.com/attachments/1491058618735394896/1508757847242964992/C451729B-CE89-4480-9D02-A0D24BAB5556.png?ex=6a16b3be&is=6a15623e&hm=e426df4f9ccfc3e125f3ea4f4a7a72fbdcc89bc1dbf27ef97cfef747ffa6f3b7&';
+
+    const BANNER = 'https://cdn.discordapp.com/attachments/1491058618735394896/1508759035216924724/51CB4E50-64FC-4009-992E-C322421DA723.png?ex=6a16b4d9&is=6a156359&hm=02fc8a8a01e77763ebede060edd082115790f78d47bbed6570c86ca67cee5ac5&';
+
+    const e = new EmbedBuilder()
+      .setColor(0x5865F2)
+      .setDescription(
+        '# <:reply:1507680110843658260> 4ccount Thread Channel\n' +
+        '### <:Boost:1508378809676861573> All private 4ccount tickets created by clients will appear under this channel. <:Amount:1501221154650853450>'
+      )
+      .setThumbnail(BOT_LOGO);
+
+    const banner = new EmbedBuilder()
+      .setColor(0x5865F2)
+      .setImage(BANNER)
+      .setFooter({ text: FOOTER_BRAND, iconURL: BOT_LOGO });
+
+    await interaction.channel.send({ embeds: [e] });
+    await interaction.channel.send({ embeds: [banner] });
+    await interaction.reply({ content: '✅ 4ccount Thread Channel panel posted.', ephemeral: true });
+  },
+};
+
+module.exports = [rankedPanelCmd, prestigePanelCmd, rankedThreadChannelCmd, prestigeThreadChannelCmd, supportThreadChannelCmd, accountThreadChannelCmd];

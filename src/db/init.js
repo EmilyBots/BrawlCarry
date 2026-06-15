cd2b025b360eac16bf0864749ee992f1e0f3cba6
rayan007-beep <rayanfautario85@gmail.com>
2026-06-15 12:12:53 +0200

    Add trophies_ticket_channel_id to guild_config migration

--- src/db/init.js
+++ src/db/init.js
@@ -146,6 +146,7 @@ async function initDb() {
     ['guild_config', 'ticket_support_roles TEXT'],
     ['guild_config', 'reviewer_roles TEXT'],
     ['guild_config', 'winstreak_ticket_channel_id BIGINT'],
+    ['guild_config', 'trophies_ticket_channel_id BIGINT'],
   ];
 
   for (const [table, colDef] of migrations) {

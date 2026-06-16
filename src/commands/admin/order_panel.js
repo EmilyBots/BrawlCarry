8d2cafdfe26a53021744cd4133449c4a12af6050
rayan007-beep <rayanfautario85@gmail.com>
2026-06-16 12:24:07 +0200

    Update embed description in order_panel.js

--- src/commands/admin/order_panel.js
+++ src/commands/admin/order_panel.js
@@ -275,7 +275,7 @@ const applicationThreadChannelCmd = {
       .setColor(0x5865F2)
       .setDescription(
         '# <:reply:1507680110843658260> Apply Thread Channel\n' +
-        '### <:Boost:1508378809676861573> All private application tickets created by members will appear under this channel <:shield:1491489447445794866>'
+        '### <:Boost:1508378809676861573> All private application tickets created by members will appear under this channel <:verified:1508838509883162786>'
       )
       .setThumbnail(BOT_LOGO);
 

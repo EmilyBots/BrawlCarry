ff5459104a6828ce429c6951b0d36b5ea3d6c45c
rayan007-beep <rayanfautario85@gmail.com>
2026-06-20 14:58:22 +0200

    Add CORS headers for API stats endpoint

--- src/api/stats.js
+++ src/api/stats.js
@@ -8,6 +8,9 @@ function startStatsServer(client) {
 
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
     next();
   });
 

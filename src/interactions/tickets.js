d7d6e03dee6f77ee3a0c00a85604bf4f1bfc121c
rayan007-beep <rayanfautario85@gmail.com>
2026-06-21 19:26:20 +0200

    Update emoji for Advertiser Application option

--- src/interactions/tickets.js
+++ src/interactions/tickets.js
@@ -274,7 +274,7 @@ new ButtonBuilder().setCustomId('ticket_close_reason_v2').setLabel('Close With R
       .addOptions(
         new StringSelectMenuOptionBuilder().setLabel('B00ster Application').setValue('apply_booster').setEmoji({ name: 'rocket', id: '1491490870979985438' }).setDescription('Apply as a b00ster'),
         new StringSelectMenuOptionBuilder().setLabel('Staff Application').setValue('apply_staff').setEmoji({ name: 'shield', id: '1491489447445794866' }).setDescription('Apply for staff team'),
-        new StringSelectMenuOptionBuilder().setLabel('Advertiser Application').setValue('apply_advertiser').setEmoji({ name: 'Carry', id: '1501221214251651082' }).setDescription('Apply as an advertiser'),
+        new StringSelectMenuOptionBuilder().setLabel('Advertiser Application').setValue('apply_advertiser').setEmoji({ name: 'announcement', id: '1508831558100975738' }).setDescription('Apply as an advertiser'),
       );
     await interaction.reply({ embeds: [e], components: [new ActionRowBuilder().addComponents(appSelect)], ephemeral: true });
 
